3e277a3694cc679eb4c136ea52643b2ae4cefc4f
Kauã Lopes <lopeskazin@gmail.com>
2024-05-28 15:31:42 -0300

    a
    a

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ function mudarcor() {
 
 }
 
+
+function capturarvalores () {
+   const nomeproj = document.getElementById('nome').value;
+   const valorhoraproj = parseFloat(document.getElementById('valorhora').value);
+   const descricaoproj = document.getElementById('descricao').value;
+   const dataproj = document.getElementById('data').value
+
+   const novoproj = {
+      nome: nomeproj,
+      data: dataproj,
+      descricao: descricaoproj,
+      valorhora: valorhoraproj
+   };
+
+   return novoproj;
+}
+
+
+
+
