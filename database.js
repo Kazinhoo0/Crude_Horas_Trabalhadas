9b4fd3f1c44aedd11b4333d3dd16a7ff8b53384a
Kauã Lopes <lopeskazin@gmail.com>
2024-05-29 16:04:45 -0300

    29/05  - 16:04
    a

--- database.js
+++ database.js
@@ -13,11 +13,12 @@ function connect() {
 }
 
 
-connect();
+
 
 
 function novo_proj() {
-    connect()
+    const connection = connect()
+    novoproj = capturarvalores()
     const novoproj = { nome:'calculadora' , data:'2024-05-28' , valorhora: 50 , descricao: 'seila' }
     connection.query('INSERT INTO projetos (nome, data, valorhora, descricao) VALUES (?,?,?,?)',
     [novoproj.nome, novoproj.data, novoproj.valorhora, novoproj.descricao], 
